83f5f805035bbedd4c1a0b23f866c154f60094b6
Nisarg Patel <nnp1997@gmail.com>
2016-10-01 19:47:35 -0400

    updated the distance_updated.py script to mask out color ranges so only one box appears at a time; added a new test image to the images folder

--- HackUMBC2016/distance_updated.py
+++ HackUMBC2016/distance_updated.py
@@ -10,8 +10,8 @@ import cv2
  
 def find_marker(image):
     # convert the image to grayscale, blur it, and detect edges
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(image, (5, 5), 0)
     edged = cv2.Canny(gray, 35, 125)
     #cv2.imshow("image1", edged)
  
@@ -29,23 +29,27 @@ def distance_to_camera(knownWidth, focalLength, perWidth):
 
 # initialize the known distance from the camera to the object, which
 # in this case is 24 inches
-KNOWN_DISTANCE = 24.0
+KNOWN_DISTANCE = 12.0
  
 # initialize the known object width, which in this case, the piece of
 # paper is 11 inches wide
 KNOWN_WIDTH = 2.0
  
 # initialize the list of images that we'll be using
-IMAGE_PATHS = ["images/image4.jpg"]
+IMAGE_PATHS = ["images/1ft.jpg", "images/2ft.jpg", "images/3ft.jpg"]
  
 # load the furst image that contains an object that is KNOWN TO BE 2 feet
 # from our camera, then find the paper marker in the image, and initialize
 # the focal length
 image = cv2.imread(IMAGE_PATHS[0])
-print image.shape
+# image = cv2.resize(image, (0,0), fx=0.2, fy=0.2)
 
-#image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
-print image.shape
+scale_red = 25
+scale_blue = 10
+scale_green = 25
+BLUE_THRESHOLDS = np.array([[200-scale_blue,200-scale_green,150-scale_red], [200+scale_blue,200+scale_green,150+scale_red]])
+image = cv2.GaussianBlur(image, (5, 5), 0)
+image = cv2.inRange(image, *BLUE_THRESHOLDS)
 
 marker = find_marker(image)
 focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
@@ -55,10 +59,16 @@ for imagePath in IMAGE_PATHS:
     # load the image, find the marker in the image, then compute the
     # distance to the marker from the camera
     image = cv2.imread(imagePath)
+    # image = cv2.resize(image, (0,0), fx=0.2, fy=0.2)
 
-    image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
+    scale_red = 25
+    scale_blue = 10
+    scale_green = 25
+    BLUE_THRESHOLDS = np.array([[200-scale_blue,200-scale_green,150-scale_red], [200+scale_blue,200+scale_green,150+scale_red]])
+    edited = cv2.GaussianBlur(image, (5, 5), 0)
+    edited = cv2.inRange(edited, *BLUE_THRESHOLDS)
 
-    marker = find_marker(image)
+    marker = find_marker(edited)
     inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
  
     # draw a bounding box around the image and display it
@@ -68,7 +78,7 @@ for imagePath in IMAGE_PATHS:
                 (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
                 2.0, (0, 255, 0), 3)
     
-    image = cv2.resize(image, (0,0), fx=0.9, fy=0.9)
+    image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
     cv2.namedWindow("image", 0)
     cv2.imshow("image", image)
     cv2.resizeWindow("image", 1920, 1080)
